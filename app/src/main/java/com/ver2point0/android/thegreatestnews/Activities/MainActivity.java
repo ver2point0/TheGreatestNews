a6bd01783550fc924dcc8399ec7f1730dc68f4f7
John McIntosh II <jmci10@hotmail.com>
2017-05-26 17:04:52 -0400

    add app styles

--- app/src/main/java/com/ver2point0/android/thegreatestnews/Activities/MainActivity.java
+++ app/src/main/java/com/ver2point0/android/thegreatestnews/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ver2point0.android.thegreatestnews;
+package com.ver2point0.android.thegreatestnews.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ver2point0.android.thegreatestnews.R;
 
 public class MainActivity extends AppCompatActivity {
 
